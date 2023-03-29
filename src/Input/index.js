23d2fd2fbfcab26aa61b467b52971c8928e523ca
Aleksey Shmelev <alexseishmelev@gmail.com>
2023-03-29 13:32:21 +0400

    useState

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../App.css";
 
-export const Input = () => {
-  const currentDate = new Date().toDateString();
+const InputTag = () => {
+  const [inputValue, setInputValue] = useState("empty");
   const handleOnChange = (event) => {
-    console.log("event", event);
-    console.log("handleOnChange");
-    console.log("value is >>>>", event.target.value);
+    setInputValue(event.target.value);
   };
-  const handleOnClick = (value) => () => {
-    console.log("handleOnClick");
-    console.log("button was clicked in", value);
-  };
+
   return (
-    <div className="InputWrap">
-      <input className="Input" onChange={handleOnChange} />
-      <button className="Button" onClick={handleOnClick(currentDate)}>
-        text
-      </button>
-    </div>
+    <input className="Input" onChange={handleOnChange} value={inputValue} />
   );
 };
+
+const BattonTag = () => <button className="Button">Add</button>;
+
+export const Input = () => (
+  <div className="InputWrap">
+    <InputTag />
+    <BattonTag />
+  </div>
+);
