7e1c5de231baf1c8364832a90b49b177caac7bf4
Aleksey Shmelev <alexseishmelev@gmail.com>
2023-03-30 14:27:52 +0400

    useRef

--- src/Input/index.js
+++ src/Input/index.js
@@ -3,10 +3,14 @@ import React, { useState, useRef } from "react";
 import "../App.css";
 
 export const Input = ({ setCitiesList }) => {
-  const [inputValue, setInputValue] = useState("empty");
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
 
   const handleOnClick = () => {
     setCitiesList((currentArrey) => [...currentArrey, inputValue]);
+    setInputValue("");
+    inputRef.current.focus();
+    console.log("inputRef", inputRef);
   };
 
   const handleOnChange = (event) => {
@@ -15,7 +19,12 @@ export const Input = ({ setCitiesList }) => {
 
   return (
     <div className="InputWrap">
-      <input className="Input" onChange={handleOnChange} value={inputValue} />
+      <input
+        className="Input"
+        onChange={handleOnChange}
+        value={inputValue}
+        ref={inputRef}
+      />
       <button className="Button" onClick={handleOnClick}>
         Add
       </button>
